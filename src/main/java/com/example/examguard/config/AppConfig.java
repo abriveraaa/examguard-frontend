d20aefb29093e6ccefdbf137fd6720285b0b639d
abriveraaa <arielbunagivera@gmail.com>
2026-05-30 09:23:03 +0800

    v1.0.2: updated the base url

--- src/main/java/com/example/examguard/config/AppConfig.java
+++ src/main/java/com/example/examguard/config/AppConfig.java
@@ -2,10 +2,8 @@ package com.example.examguard.config;
 
 public class AppConfig {
 
-    private AppConfig() {
-    }
+    private AppConfig() {}
 
     public static final String BASE_URL = "https://examguard-backend-production.up.railway.app";
 
-//    public static final String BASE_URL = "http://localhost:8080";
 }
