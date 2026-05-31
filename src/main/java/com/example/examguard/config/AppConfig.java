ca62cbc2e4cfe07490d135c45c3fa6c0894fc6e5
abriveraaa <arielbunagrivera@gmail.com>
2026-05-31 09:13:01 +0800

    test

--- src/main/java/com/example/examguard/config/AppConfig.java
+++ src/main/java/com/example/examguard/config/AppConfig.java
@@ -4,6 +4,6 @@ public class AppConfig {
 
     private AppConfig() {}
 
-//    public static final String BASE_URL = "https://examguard-backend-production.up.railway.app";
-    public static final String BASE_URL = "http://localhost:8080";
+    public static final String BASE_URL = "https://examguard-backend-production.up.railway.app";
+//    public static final String BASE_URL = "http://localhost:8080";
 }
